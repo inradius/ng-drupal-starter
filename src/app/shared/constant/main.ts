7bf6f60fd96f1a46256deb93bd7ca42b8d2146b2
Travis Stroud <tstroud@magiclogix.com>
2017-08-08 08:51:43 -0500

    Use Drupal's runserver localhost and port for api

--- src/app/shared/constant/main.ts
+++ src/app/shared/constant/main.ts
@@ -2,8 +2,7 @@ export const MAIN: any = {
     APP: {
         BRAND: 'ng-drupal-starter App',
         DRUPAL_API : {
-            API_SERVER: 'http://localhost:9000',
-            LOGIN_REDIRECT: 'http://admin.beauticontrol.dev/admin'
+            API_SERVER: 'http://localhost:8888'
         }
     },
 };
